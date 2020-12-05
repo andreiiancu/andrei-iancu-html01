08e043ae603f656fa25eb9f43265e0e18f07aa68
andreiiancu <65896820+andreiiancu@users.noreply.github.com>
2020-12-06 01:11:51 +0200

    Protopopiat/header

--- agnita/Protopopiatul Agnita/html/app.js
+++ agnita/Protopopiatul Agnita/html/app.js
@@ -1,11 +1,36 @@
-$(document).ready(function() {
-    $('.first-button').on('click', function() {
-        $('.animated-icon1').toggleClass('open');
-    });
-    $('.second-button').on('click', function() {
-        $('.animated-icon2').toggleClass('open');
-    });
-    $('.third-button').on('click', function() {
-        $('.animated-icon3').toggleClass('open');
-    });
+const h1 = document.getElementsByTagName('hmenuBurger')[0];
+const ul = document.getElementsByTagName('ul')[0];
+const breakPoint = 500;
+
+const clickHandler = () => {
+  const displayValue = getComputedStyle(ul).display;
+
+  if (displayValue === 'block') {
+    ul.style.display = 'none';
+  }
+
+  if (displayValue === 'none') {
+    ul.style.display = 'block';
+  }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const width = window.innerWidth;
+
+  if (width <= breakPoint) {
+    menuBurger.addEventListener('click', clickHandler);
+    ul.style.display = 'none';
+  }
+});
+
+window.addEventListener('resize', () => {
+  const width = window.innerWidth;
+
+  if (width <= breakPoint) {
+    menuBurger.addEventListener('click', clickHandler);
+    ul.style.display = 'none';
+  } else {
+    menuBurger.removeEventListener('click', clickHandler);
+    ul.style.display = 'block';
+  }
 });
