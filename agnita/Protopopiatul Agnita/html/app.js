8a0751df1ada012384a9d8edc6444d678548cde5
andreiiancu <65896820+andreiiancu@users.noreply.github.com>
2020-12-09 16:22:41 +0200

    protopopiat

--- agnita/Protopopiatul Agnita/html/app.js
+++ agnita/Protopopiatul Agnita/html/app.js
@@ -1,36 +1,8 @@
-const HumburgerElement = document.getElementsByClassName('humburger')[0];
-const ulElement = document.getElementsByClassName('menutop')[0];
-const breakPoint = 900;
-
-const clickHandler = () => {
-  const displayValue = getComputedStyle(ulElement).display;
-
-  if (displayValue === 'block') {
-    ulElement.style.display = 'none';
-  }
-
-  if (displayValue === 'none') {
-    ulElement.style.display = 'block';
-  }
-};
-
-document.addEventListener('DOMContentLoaded', () => {
-  const width = window.innerWidth;
-
-  if (width <= breakPoint) {
-    HumburgerElement.addEventListener('click', clickHandler);
-    ulElement.style.display = 'none';
-  }
-});
-
-window.addEventListener('resize', () => {
-  const width = window.innerWidth;
-
-  if (width <= breakPoint) {
-    HumburgerElement.addEventListener('click', clickHandler);
-    ulElement.style.display = 'none';
+function myFunction() {
+  var x = document.getElementById('myLinks');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
   } else {
-    HumburgerElement.removeEventListener('click', clickHandler);
-    ulElement.style.display = 'block';
+    x.style.display = 'block';
   }
-});
+}
