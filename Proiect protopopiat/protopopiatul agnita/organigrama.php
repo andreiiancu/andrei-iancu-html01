78d09f7ad152017e89f3dcc143d81b0e14dfa52e
andreiiancu <65896820+andreiiancu@users.noreply.github.com>
2021-01-12 23:09:36 +0200

    organigrama

--- Proiect protopopiat/protopopiatul agnita/organigrama.php
+++ Proiect protopopiat/protopopiatul agnita/organigrama.php
@@ -1,150 +1,164 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Cancelaria Protopopiatului Agnita</title>
-</head>
-<body>
-  <br> <br> <br>
-  <?php
-    include 'headerparohii.php';
-  ?>
-    <!-- ======= Parohii ======= -->
-    <section
-      id="team"
-      class="team section-bg"
-    >
-      <div
-        class="container"
-        data-aos="fade-up"
-      >
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
 
-        <div class="section-title">
-          <a href="">
-            <h2 class=" border-bg-danger text-danger">Organigrama</h2>
-          </a>
-          <h3>Cancelaria <span>Protopopiatului Agnita</span></h3>
-        </div>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-        <div class="row">
+<section class="our-webcoderskull padding-lg">
+  <div class="container">
+    <div class="row heading heading-icon">
+        <h2>Cancelaria Protopopiatului Agnita</h2>
+    </div>
+    <ul class="row">
+      <li class="col-12 col-md-6 col-lg-3">
+          <div class="cnt-block equal-hight" style="height: 349px;">
+            <figure><img src="../protopopiatul agnita/assets/img/preoti/pr naicu.jpg" class="img-responsive" alt=""></figure>
+            <h3><a href="http://www.webcoderskull.com/">Pr. Naicu Mihai</a></h3>
+            <p>PROTOPOP</p>
 
-          <div
-            class="col-lg-3 col-md-6 d-flex align-items-stretch"
-            data-aos="fade-up"
-            data-aos-delay="100"
-          >
-            <div class="member">
-              <div class="member-img">
-                <img
-                  src="../protopopiatul agnita/assets/img/preoti/pr naicu.jpg"
-                  class="img-fluid"
-                  alt=""
-                >
-                <!-- <div class="social">
-                  <a href=""><i class="icofont-twitter"></i></a>
-                  <a href=""><i class="icofont-facebook"></i></a>
-                  <a href=""><i class="icofont-instagram"></i></a>
-                  <a href=""><i class="icofont-linkedin"></i></a>
-                </div> -->
-              </div>
-              <div class="member-info">
-                <h4>Pr. Naicu Mihai</h4>
-                <h6>Funcția: Protopop</h6>
-                <a class="text-dark" href="tel:+">0764568973</a>
-              </div>
-            </div>
           </div>
+      </li>
+      <li class="col-12 col-md-6 col-lg-3">
+          <div class="cnt-block equal-hight" style="height: 349px;">
+            <figure><img src="../protopopiatul agnita/assets/img/preoti/pr Andrei.gif" class="img-responsive" alt=""></figure>
+            <h3><a href="#">Pr. Iancu Andrei </a></h3>
+            <p>SECRETAR</p>
 
-          <div
-            class="col-lg-3 col-md-6 d-flex align-items-stretch"
-            data-aos="fade-up"
-            data-aos-delay="200"
-          >
-           <div class="member">
-              <div class="member-img">
-                <img
-                  src="../protopopiatul agnita/assets/img/preoti/pr naicu.jpg"
-                  class="img-fluid"
-                  alt=""
-                >
-                <!-- <div class="social">
-                  <a href=""><i class="icofont-twitter"></i></a>
-                  <a href=""><i class="icofont-facebook"></i></a>
-                  <a href=""><i class="icofont-instagram"></i></a>
-                  <a href=""><i class="icofont-linkedin"></i></a>
-                </div> -->
-              </div>
-              <div class="member-info">
-                <h4>Leb Angela</h4>
-                <h6>Funcția: Contabil</h6>
-                <a class="text-dark" href="tel:+">0764568973</a>
-              </div>
-            </div>
           </div>
-
-          <div
-            class="col-lg-3 col-md-6 d-flex align-items-stretch"
-            data-aos="fade-up"
-            data-aos-delay="300"
-          >
-            <div class="member">
-              <div class="member-img">
-                <img
-                  src="../protopopiatul agnita/assets/img/preoti/pr naicu.jpg"
-                  class="img-fluid"
-                  alt=""
-                >
-                <!-- <div class="social">
-                  <a href=""><i class="icofont-twitter"></i></a>
-                  <a href=""><i class="icofont-facebook"></i></a>
-                  <a href=""><i class="icofont-instagram"></i></a>
-                  <a href=""><i class="icofont-linkedin"></i></a>
-                </div> -->
-              </div>
-              <div class="member-info">
-                <h4>Naicu Angela</h4>
-                <h6>Funcția:Responsabil Magazin Arhidiecezana</h6>
-                <a class="text-dark" href="tel:+">0764568973</a>
-              </div>
-            </div>
+      </li>
+      <li class="col-12 col-md-6 col-lg-3">
+          <div class="cnt-block equal-hight" style="height: 349px;">
+            <figure><img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt=""></figure>
+            <h3><a href="http://www.webcoderskull.com/">Leb Angela </a></h3>
+            <p>CONTABIL</p>
           </div>
-
-          <div
-            class="col-lg-3 col-md-6 d-flex align-items-stretch"
-            data-aos="fade-up"
-            data-aos-delay="400"
-          >
-            <div class="member">
-              <div class="member-img">
-                <img
-                  src="../protopopiatul agnita/assets/img/preoti/pr Andrei.gif"
-                  class="img-fluid"
-                  alt=""
-                >
-                <!-- <div class="social">
-                  <a href=""><i class="icofont-twitter"></i></a>
-                  <a href=""><i class="icofont-facebook"></i></a>
-                  <a href=""><i class="icofont-instagram"></i></a>
-                  <a href=""><i class="icofont-linkedin"></i></a>
-                </div> -->
-              </div>
-              <div class="member-info">
-                <h4>Pr. Iancu Andrei</h4>
-                <h6>Funcția: Secretar</h6>
-                <a class="text-dark" href="tel:+">0751337979</a>
-              </div>
-            </div>
+       </li>
+      <li class="col-12 col-md-6 col-lg-3">
+          <div class="cnt-block equal-hight" style="height: 349px;">
+            <figure><img src="http://www.webcoderskull.com/img/team2.png" class="img-responsive" alt=""></figure>
+            <h3><a href="http://www.webcoderskull.com/">Naicu Angela </a></h3>
+            <p>RESPONSABIL MAGAZIN ARHIDIECEZANA</p>
           </div>
+      </li>
+    </ul>
+  </div>
+</section>
 
-        </div>
+<style>
+  .row.heading h2 {
+    color: #fff;
+    font-size: 52.52px;
+    line-height: 95px;
+    font-weight: 400;
+    text-align: center;
+    margin: 0 0 40px;
+    padding-bottom: 20px;
+    text-transform: uppercase;
+}
+ul{
+  margin:0;
+  padding:0;
+  list-style:none;
+}
+.heading.heading-icon {
+    display: block;
+}
+.padding-lg {
+	display: block;
+	padding-top: 60px;
+	padding-bottom: 60px;
+}
+.practice-area.padding-lg {
+    padding-bottom: 55px;
+    padding-top: 55px;
+}
+.practice-area .inner{
+     border:1px solid #999999;
+	 text-align:center;
+	 margin-bottom:28px;
+	 padding:40px 25px;
+}
+.our-webcoderskull .cnt-block:hover {
+    box-shadow: 0px 0px 10px rgba(0,0,0,0.3);
+    border: 0;
+}
+.practice-area .inner h3{
+    color:#3c3c3c;
+	font-size:24px;
+	font-weight:500;
+	font-family: 'Poppins', sans-serif;
+	padding: 10px 0;
+}
+.practice-area .inner p{
+    font-size:14px;
+	line-height:22px;
+	font-weight:400;
+}
+.practice-area .inner img{
+	display:inline-block;
+}
 
-      </div>
-    </section><!-- End Parohii -->
 
+.our-webcoderskull{
+  background: url("../Protopopiatul Agnita/assets/img/team/RWg5f1.png") no-repeat center top / cover;
 
-  <?php
-    include 'footer.php';
-  ?>
-</body>
-</html>
+}
+.our-webcoderskull .cnt-block{
+   float:left;
+   width:100%;
+   background:#fff;
+   padding:30px 20px;
+   text-align:center;
+   border:2px solid #d5d5d5;
+   margin: 0 0 28px;
+}
+.our-webcoderskull .cnt-block figure{
+   width:148px;
+   height:148px;
+   border-radius:100%;
+   display:inline-block;
+   margin-bottom: 15px;
+}
+.our-webcoderskull .cnt-block img{
+   width:148px;
+   height:148px;
+   border-radius:100%;
+}
+.our-webcoderskull .cnt-block h3{
+   color:#2a2a2a;
+   font-size:20px;
+   font-weight:500;
+   padding:6px 0;
+   text-transform:uppercase;
+}
+.our-webcoderskull .cnt-block h3 a{
+  text-decoration:none;
+	color:#2a2a2a;
+}
+.our-webcoderskull .cnt-block h3 a:hover{
+	color:#337ab7;
+}
+.our-webcoderskull .cnt-block p{
+   color:#2a2a2a;
+   font-size:13px;
+   line-height:20px;
+   font-weight:400;
+}
+.our-webcoderskull .cnt-block .follow-us{
+	margin:20px 0 0;
+}
+.our-webcoderskull .cnt-block .follow-us li{
+    display:inline-block;
+	width:auto;
+	margin:0 5px;
+}
+.our-webcoderskull .cnt-block .follow-us li .fa{
+   font-size:24px;
+   color:#767676;
+}
+.our-webcoderskull .cnt-block .follow-us li .fa:hover{
+   color:#025a8e;
+}
+
+</style>
