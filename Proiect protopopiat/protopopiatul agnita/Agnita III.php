46daa93615320c3be7c7c48135317e1dfd71b355
andreiiancu <65896820+andreiiancu@users.noreply.github.com>
2021-01-17 16:53:13 +0200

    agnita 3

--- Proiect protopopiat/protopopiatul agnita/Agnita III.php
+++ Proiect protopopiat/protopopiatul agnita/Agnita III.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Parohia Agnita II</title>
+  <title>Parohia Agnita III</title>
 </head>
 <body>
   <?php
@@ -30,9 +30,11 @@ include_once 'headerparohii.php';
         border-bottom: 2px red solid;
         "
           > Protopopiatul Ortodox Agnita</h2>
-          <h3><span>Parohia Agnita II</span></h3>
-          <p>Biserica "Sf. Nicolae"</p>
-       </div>
+          <h3><span>Parohia Agnita III</span></h3>
+          <p>
+           Biserica  <i>Sfinţii Apostoli Petru şi Pavel</i>
+          </p>
+        </div>
 
        <!-- contact -->
         <div
@@ -47,7 +49,7 @@ include_once 'headerparohii.php';
             <div class="info-box mb-4">
               <i class="bx bx-map w-100"></i>
               <h3>Adresa</h3>
-              <p>str. Griviţa, nr.4, Agnita, jud. Sibiu</p>
+              <p>str. Grădinilor, nr. 5, Agnita, jud. Sibiu</p>
             </div>
           </div>
 
@@ -56,7 +58,7 @@ include_once 'headerparohii.php';
               <i class="bx bx-envelope"></i>
               <h3>Email</h3>
                 <a class="text-secondary" href="mailto:">
-                tnt_florin@yahoo.comm</a>
+                tnt_florin@yahoo.com</a>
              </div>
           </div>
 
@@ -68,7 +70,7 @@ include_once 'headerparohii.php';
               <h3 class="ml-5">Sună</h3>
 
               <a class="text-secondary ml-5 w-60" href="tel:+">
-                0269.518151</a>
+                </a>
             </div>
             </div>
             </div>
@@ -76,13 +78,13 @@ include_once 'headerparohii.php';
 
         <div class="row">
           <div
-            class="col-lg-12 "
+            class="col-lg-12"
             data-aos="zoom-out"
             data-aos-delay="100"
           >
             <img
               src="assets/img/poze parohii/Agnita II/f5d93eca-5ae6-4330-bdb2-24594d282984.jpg"
-              class="img-fluid col-lg-12 " style="height:50rem"
+              class="img-fluid col-lg 12"
               alt=""
             > <span class="text-danger"><i>FOTO: Catedrala "Sfinţii Împăraţi Constantin şi Elena" Agnita</i></span>
           </div>
@@ -168,7 +170,7 @@ include_once 'headerparohii.php';
             <div class="our-team">
                 <img src="../protopopiatul agnita/assets/img/preoti/pr naicu.jpg">
                 <div class="team-content">
-                    <h3 class="title">Pr Ţânţ Florin</h3>
+                    <h3 class="title">Pr Mărginean Nicolae</h3>
                     <span class="post">Doamne Ajută!</span>
                     <ul class="social">
 
@@ -280,166 +282,76 @@ include_once 'headerparohii.php';
 </div>
           <!--=====end-foto-priest=====-->
 
+<br> <br>
+      <h6 class="text-indent: 2rem">A. Istoricul comunității parohiale</h6>
+        <p style="text-indent: 2rem;">
+         <i>Numele parohiei şi protopopiatul din care provine:</i>  Parohia Agnita III face parte din Potopopiatul Agnita şi a fost înfiinţată în anul 1990 având în componenţă următorele străzi: Fabricii, Spitalului, Mihai Viteazul, Piaţa Republicii şi Dealului.
+       </p>
 
-             <br> <br>
+        <p style="text-indent: 2rem;">
+          <i>Numele localităţii plus toponimie şi prezentarea generală a localităţii (geografic, istorico-demografic, archeologic, cultural, economic)</i> , vezi <a class="btn badge-danger text-light" href="../protopopiatul agnita/agnita I.php">Agnita I</a>.
+       </p>
 
-            <h6 style="line-height: 1.2";>A. Istoricul comunității parohiale</h6>
+        <p style="text-indent: 2rem;">
+          <i> Prezentarea generală a localităţii (geografic, istorico-demografic, arheologic, cultural, economic)</i> : vezi Agnita I.
+       </p>
 
+       <h6 class="text-indent: 2rem">B. Istoricul bisericii parohiale.</h6>
+       <p style="text-indent: 2rem;">
+         <i>Istoria zidirii ei:</i>  Biserica nouă, zidirea ei a început în anul 2005.
+          Arhitectura (plan, dimensiuni, material de construcţie), navă cu elemente bizantine, 218 mp, ciment, cărămidă mica, lemn şi ţiglă.
+       </p>
 
+       <p style="text-indent: 2rem;">
+         <i>Istoria zidirii ei:</i> Este vorba despre biserica nouă, a cărei zidire a început în anul 2005.
+          Arhitectura (plan, dimensiuni, material de construcţie), navă cu elemente bizantine, 218 mp, ciment, cărămidă mica, lemn şi ţiglă.
+       </p>
 
-          <p class=" text-justify "
-            style="text-align:justify;
-              text-indent: 5ch;
-           "
-            >
+        <p style="text-indent: 2rem;">
+          <i>Arhitectura (plan, dimensiuni, material de construcţie)</i> : navă cu elemente bizantine, 218 mp, ciment, cărămidă mica, lemn şi ţiglă.
+       </p>
 
-              Parohia Agnita II face parte din Potopopiatul  Agnita şi a fost înfiinţată în anul 1972, având în componenţă următorele străzi: Bisericii, Griviţa, Progrăzii, Noua, Plevna, Mihai Eminescu, Smârdan, Aleea Teilor, Abatorului şi  Floreasca.
-              Numele localităţii plus toponimie şi prezentarea generală a localităţii (geografic, istorico-demografic, archeologic, cultural, economic), vezi Agnita I
+        <p style="text-indent: 2rem;">
+          <i>Obiecte vechi de cult:</i> nu este cazul
+       </p>
 
-           </p>
+       <p class="col-lg-12" style="text-indent: 1rem;">
+        <i>Manuscrise şi cărţi vechi</i>: nu este cazul
+       </p>
 
+        <p style="text-indent: 1rem;">
+          <i>Pomelnice şi alte înscrieri sau inscripţii:</i>  nu este cazul
+       </p>
 
+        <h6 class="text-indent: 1rem col-lg-12">C. Cimitirul</h6>
 
+        <p class="col-lg-12" style="text-indent: 1rem;">
+          Cimitir parohial comun cu parohiile Agnita I şi III.
+        </p>
 
+         <h6 class="text-indent: 1rem col-lg-12">D. Activităţi culturale şi filantropice în trecut: </h6>
 
-            <br>
-            <h6 style="line-height: 1.2";>B. Istoricul bisericii parohiale.</h6>
-          <p style="text-indent: 5ch;
-              ">
-             Documente şi ştiri despre organizarea bisericească a românilor ortodocşi din Agnita avem odată cu zidirea bisericii "Sf. Nicolae".
-             În peretele dinspre miazăzi în Sf. Altar a fost pusă o piatră în care a fost săpată o inscripţie, care face dovada începerii lucrărilor la biserică şi cine sunt ctitorii ei.
-             Datorită igrasiei din perete piatra a suferit stricăciuni (corodare), ea fiind scoasă din locul ei şi păstrată în colecţia de obiecte bisericesti de la casa parohială. Iata inscripţia atât cât s-a mai păstrat: "În anul 1795, 25 aprilie s-au ridicat aceasta biserică de lege grecească, neunită în hram S.Nicolae, prin cheltuiala d-lor. jupân Dumitru Orghidan, dom(n) burger din cet(atea) Braşov şi d-(u)nealui Kir Radu Bogdan la(cuitor) in Bo(l)ga(r)sec "
-             Ctitorii acestei biserici sunt doi braşoveni cu dare de mână Dumitru Orghidan si Radu Bogdan, care au mai ridicat si alte biserici în Ardeal (vreo 10, pe valea Hârtibaciului mai este o ctitorie a lor - biserica din Movile).
-            </p>
+        <p class="col-lg-12" style="text-indent: 1rem;">
+          Nu este cazul
+        </p>
 
-           <p style="text-indent: 5ch;">
-              Biserica este zidită din piatră şi cărămidă. În exterior are forma de corabie, iar în interior respectă stilul bizantin cu cupolă si boltă în altar, naos si pronaos.
-              Primul preot ortodox român la Agnita  despre care vorbesc documentele este Ignatie Crăciun Tatarovici. În timpul păstoririi sale s-a zidit biserica. Era originar din Sighişoara şi a fost hirotonit la Buda în anul 1782. Biserica a fost sfinţită în iulie 1821 de către episcopul Vasile Moga. Accesul la biserică se facea prin curtea casei parohiale (azi strada Grivita nr. 4) și prin partea de sus a bisericii (azi strada Plevna).
-              Ioan Moldovan  este al doilea preot cunoscut cu numele la Agnita care a păstorit între anii 1834-1852.
-              Al treilea preot a fost un fiu al preotului Ioan Moldovan cu numele tot de Ioan Moldovan, între anii 1854-1873.
-              Din anul 1873 pana la 1880 parohia Agnita a fost "administrată" de preoţii Ioachim Părău din Vărd (1873-1879) şi Alexandru Comşa din Coveş (1879-1880)
-              În anul 1880 parohienii au ales ca preot paroh la Agnita pe ginerele fostului paroh Ioan Moldovan, care funcţiona în Vărd- Ioachim Parau.
-           </p>
+       <h6 class="text-indent: 1rem col-lg-12">E. Profilul actual al parohiei. </h6>
 
-            <p style="text-indent: 5ch;">
-              Consistoriul a aprobat alegerea făcută de credincioşii agniteni, dar nu ca paroh ci ca şi capelan protopopesc până  în anul 1926.
-              În anul 1882 sub păstorirea preotului Ioachim Părău s-a primit ca donaţie din partea credinciosului Gheorghe Radu o fâşie din gradina acestuia, din care s-a făcut intrarea principală la biserică dinspre strada Bisericii. În 1890 biserica a fost renovată, iar zidurile au fost legate cu 7 chei de fier.
-              În anul 1906 preotul  Ioachim Munteanu a fost instalat ca protopop si paroh al parohiei Agnita de către comisarul consistorial asesorul Matei Voileanu pe data de 20 aprilie 1906.
-           </p>
+        <p class="col-lg-12" style="text-indent: 1rem;">
+          Numărul de credincioşi:1.450
+        </p>
 
-            <p style="text-indent: 5ch;">
-              Protopopul Ioachim Munteanu a avut o activitate extrem de bogată şi putem spune că a reuşit să aşeze instituţia pe care a condus-o aproape două decenii pe un făgaş normal. La 2 iulie 1925, după o activitate fructuoasă şi îndelungată, a părăsit Agnita, stabilindu-se la Sibiu.
-              După plecarea lui Ioachim Munteanu, mitropolitul Nicolae Bălan l-a numit ca administrator protopopesc la Agnita pe preotul Ioan Alexandru din Ilimbav.
-              De la 1 iulie 1925 şi până la sfărşitul aceluiaşi an Ioan Alexandru mergea de 3-4 ori pe săptămână la Agnita, pentru a se ocupa de problemele tractului. Capelanul protopopesc de atunci, Ioachim Părău, socrul lui Ioan Alexandru, era deja în vârstă de 85 de ani şi nu mai putea face faţă problemelor cu care se confrunta Agnita pe tărâm bisericesc. De aceea mitropolitul Nicolae Bălan i-a propus lui Ioan Alexandru să se mute la Agnita şi să administreze şi parohia de acolo, urmând ca parohia Ilimbav să fie administrată de către preoţii din împrejurimi. Acest lucru s-a şi întâmplat începând cu ianuarie 1926. La scurt timp după aceea, în luna februarie, a decedat capelanul Ioachim Părău. În această nouă situaţie, Ioan Alexandru a rămas şi administrator protopopesc şi administrator parohial, locuind în casele tractului până în anul 1928, când a venit noul protopop şi paroh, Emil Păcală.
-              La 8 noiembrie 1928 a avut loc instalarea noului titular al postului de protopop- pr. Emil Păcală, acesta fiind din Dealu Frumos.
-              În timpul păstoririi sale, în 1939, au început lucrările de construire a bisericii cu hramul Sfinţii Împăraţi Constantin şi Elena.
-              În anul 1945 protopopul Emil Păcală s-a pensionat, iar locul său a fost luat de preotul Chirion Marcu, care a continuat lucrările de construire a noii biserici din Agnita.
-              Din 1948, după construirea noii biserici, până în 1953, preoţii au slujit numai în biserica nou construită.
-           </p>
-
-            <p style="text-indent: 5ch;">
-              În perioada 1953-1959 s-a slujit prin rotaţie la ambele biserici, dar din anul 1960 până în anul 1972 nu s-a mai slujit în biserica Sf. Nicolae.
-              Cu venirea în anul 1972 a preotului Săsăujan Simion se poate vorbi despre Parohia  Agnita I, cu biserica Sf. Împărați Constantin si Elena şi Parohia Agnita II, cu biserica Sfântul Nicolae.
-              Pr. Simion Săsăujan a repus în stare de funcţionare biserica Sfântul Nicolae. Pe vremea sa , s-a îmbrăcat biserica într-o frumoasă haină picturală, s-a dotat cu iconostas sculptat din stejar masiv şi cu strane, executate la atelierele Partriarhiei din Bucuresti. Aceste lucrări au fost binecuântate  de către vrednicul de pomenire mitropolitul Antonie, în anul 1982.
-           </p>
-
-            <p style="text-indent: 5ch;">
-              Tot în timpul păstoririi preotului  Săsăujan Simion, s-au adus unele îmbunatăţiri fostei şcoli confesionale, devenită casa parohială în care s-a organizat o colecţie de obiecte bisericeşti.
-              Din 1990 a fost numit prin transfer de la Parohia  Coves , preotul Ioan Jurca.
-              Între anii 1994-1995, cladirii bisericii i s-a mai adaugat un nou edificiu, clopotniţa în care s-au ridicat cele două clopote cu care era înzestrată biserica la care s-a mai adăugat unul mai mare, donat de un fiu al parohiei, plecat cu ani în urmă peste ocean.
-              Aceste lucrari au fost încununate prin binecuvântarea lor de către mitropolitul Antonie Plămădală, în ziua de 25 aprilie 1995 (a treia zi de Pasti), exact la 200 de ani de la punerea pietrei de temelie a bisericii S.Nicolae.
-              Biserica Sfântul Nicolae, alături de prima şcoală românească şi de colecţia de obiecte bisericeşti au devenit parte componentă a Patrimoniului Național Cultural şi puncte turistice pentru agniteni și vizitatorii străini.
-              Preoţi slujitori: Ignatie Crăciun Tatarovici  ? – 1834; Ioan Moldovan 1834-1852; Ioan Grecu 1852-1854; Ioan Moldovan, fiul 1854-1873; Ioachim Părău 1873-1879; Alexandru Comşa 1879-1880; Ioachim Părău 1880-1926; Sabin Piso 1886-1896; Ioan Alexandru 1925-1928; Emil Păcală 1928-1945; Emil Pora 1930-1959; Chirion Marcu 1945-1950; Simion Săsăujan 1972-1990; Ioan Jurca 1990-prezent.
-           </p>
-
-          <br>
-          <h6 style="line-height: 1.2";>Arhitectura (plan, dimensiuni, material de construcţie)":</h6>
+        <p class="col-lg-12" style="text-indent: 1rem;">
+        <i>Activităţi pastoral-misionare, culturale, editorial, filantropice, catehetice ş.a.:</i>   programul catehetic naţional “Hristos împărtăşit copiilor”; proiectul educativ naţional “Alege şcoala!”; participarea la etapa pe protopopiat a concursurilor naţionale de miniproiecte cu caracter misionar-filantropic organizate anual în întreaga Patriarhie Română; sprijinirea în măsura posibilităţilor a persoanelor vârstnice şi a celor care se confruntă cu grave probleme sociale.
+        </p>
 
 
-           <p style="text-indent: 5ch;">Forma clădirii este de cruce greacă, cu coloane şi multe ferestre în pereţi.
-             Stilul arhitectonic este cel bizantin, fapt vizibil încă de la prima vedere prin existenţa a trei cupole, dintre care una este fixată pe locul de întretăiere a celor două braţe ale crucii.
-             Materialul din care este construită constă în cărămidă, beton armat, lemn şi tablă de aluminiu.
-             În interior bolta centrală este continuată în forma crucii de patru abside semicirculare sprijinite pe pereţi.
-             Pardoseala este din ciment şi mozaic. În subsol are un coridor şi un loc amenajat în vederea unei instalaţii de încălzire centrală.
-           </p>
-
-          <br>
-          <h6 style="line-height: 1.2";>C. Cimitirul</h6>
-
-            <p style="text-indent: 5ch;">
-              Cimitir parohial comun cu parohiile Agnita I şi III.
-            </p>
-
-          <br>
-          <h6 style="line-height: 1.2";>D. Activități culturale și filantropice în trecut:</h6>
-
-          <p style="text-indent: 5ch;">
-            Dând curs prevederilor Legii 63/1974 privitoare la patrimoniul cultural-naţional, cu acordul autorităţilor bisericeşti şi de
-            stat, în casa în care a funcţionat vechea şcoală românească din oraş (acum casă parohială) s-a înfiinţat un mic muzeu , care
-            cuprinde o microcolecţie de icoane şi alte obiecte de cult (cărţi, cruci, potire). Muzeul a fost inaugurat cu prilejul conferinţei
-            de orientare din luna mai 1978, în prezenţa mitropolitului Nicolae Mladin. Obiectele care fac parte din colecţia Muzeului bisericesc
-            de la parohia Agnita II din Agnita sunt următoarele: 319 cărţi vechi,108 icoane pe sticlă şi 17 de icoane; alte obiecte (cruci, potire,
-            antimise, între care şi un antimis din vremea păstoririi lui Dionisie Novacovici, datat 1762). Icoanele pe sticlă provin din diferitele centre
-            de creaţie artistică de acest gen din Transilvania (zona Nicula, zona Maramureşului, zona Ţării Făgăraşului). De o deosebită valoare artistică
-            şi istorică sunt uşile împărăteşti pictate, rămase din vechiul iconostas al bisericii Sfântul Nicolae din Agnita, precum şi un chivot pictat,
-            ambele datând de la sfârşitul secolului al XVIII-le Muzeul bisericesc din Agnita, prin piesele pe care le deţine în colecţia sa, atestă într-un mod
-            specific vechimea, continuitatea şi intensitatea credinţei creştine a locuitorilor din această zonă.
-          </p>
-
-          <br>
-          <h6 style="line-height: 1.2";>E. Profilul actual al parohiei:</h6>
-
-          <p style="text-indent: 5ch;">
-            Numărul de credincioşi: 1.540
-          </p>
-          <p style="text-indent: 5ch;">
-            Activităţi pastoral-misionare, culturale, editorial, filantropice, catehetice ş.a. programul catehetic naţional “Hristos împărtăşit
-            copiilor”; proiectul educativ naţional “Alege şcoala!”; participarea la etapa pe protopopiat a concursurilor naţionale de miniproiecte
-            cu caracter misionar-filantropic organizate anual în întreaga Patriarhie Română; sprijinirea în măsura posibilităţilor a persoanelor
-            vârstnice şi a celor care se confruntă cu grave probleme sociale
-          </p>
 
 
-          <br>
-          <h6 style="line-height: 1.2";>Surse bibliografice:</h6>
 
-          <p style="text-indent: 5ch;">
-          <ul style="list-style-type: none;">
-            <li>
-                1.	Arhiva parohială
-            </li>
 
-            <li>
-                2.	Arhiva  Muzeului „Valea Hârtibaciului” din Agnita
-            </li>
 
-            <li>
-                3. Nicolae Stoicescu – Repertoriul bibliografic al localităţilor şi monumentelor medievale din Transilvania , Ed. Andreiană, vol I, Sibiu 2013.
-            </li>
 
-            <li>
-                4.	Dr. Florina Grecu -  Agnita , Mic îndreptar turistic, Ed. Sport - Turism , Bucuresti 1988
-            </li>
-
-            <li>
-                5.	Ioachim Munteanu, Crâmpeie din viaţa protopopului ortodox român Ioachim Munteanu, Tiparul Tipografiei „Săteanului”, Sibiu, 1933.
-            </li>
-
-            <li>
-                6. Pr. Simion Săsăujan, „Colecţia de la parohia Agnita II, jud. Sibiu”, în Arhiepiscopia Sibiului – pagini de istorie, Sibiu, 1981, p. 254-257.
-            </li>
-
-            <li>
-                7. George Togan, „Prima atestare documentară a Agnitei. Despre documentul din 1280”, text dactilografiat, redactat în 1980 şi aflat în Arhiva Muzeului „Valea Hârtibaciului” din Agnita.
-            </li>
-          </ul>
-
-          </p>
-
-          <p style="text-indent: 5ch;">
-          <h6 style="line-height: 1.2";>Adresa:</h6>
-              Str. Griviţa, nr.4, Agnita, jud. Sibiu, tel. 0269510921
-          </p>
 </div>
 
           </div>
@@ -527,7 +439,7 @@ include_once 'headerparohii.php';
           <li class="carousel_slide">
             <img
               class="carousel_image"
-              src="../protopopiatul agnita/assets/img/poze parohii/Agnita II/f5d93eca-5ae6-4330-bdb2-24594d282984.jpg"
+              src="../protopopiatul agnita/assets/img/poze parohii"
               alt=""
               width="90px"
             >
@@ -710,5 +622,8 @@ include_once 'headerparohii.php';
 include_once 'footer.php';
 ?>
 
+<script>
+
+</script>
 </body>
 </html>
