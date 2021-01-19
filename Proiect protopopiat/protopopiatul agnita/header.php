88b34fe9629623d183c4e5ea4b882d874a73b1dc
andreiiancu <65896820+andreiiancu@users.noreply.github.com>
2021-01-19 04:23:48 +0200

    parohii

--- Proiect protopopiat/protopopiatul agnita/header.php
+++ Proiect protopopiat/protopopiatul agnita/header.php
@@ -311,10 +311,205 @@
   </header><!-- End Header -->
     <!-- ======= Hero Section ======= -->
   <section
+  class="hero"
     id="hero"
     class="d-flex align-items-center"
   >
-    <div
+
+<!-- text-effect -->
+<div style="margin-top: 15rem;   margin-left: 5rem;
+" >
+<h1 class="ml15 text-primary">
+  <span class="word">Protopopiatul </span>
+  <span class="word"> Ortodox</span>
+  <span class="word">Român</span>
+  <span class="word"> Agnita</span>
+</h1>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+
+<style>
+  .ml15 {
+  font-weight: 800;
+  font-size: 3.8em;
+  text-transform: uppercase;
+  letter-spacing: 0.2em;
+}
+
+.ml15 .word {
+  display: inline-block;
+  line-height: 1em;
+}
+</style>
+
+<script>
+ anime.timeline({loop: true})
+  .add({
+    targets: '.ml15 .word',
+    scale: [14,1],
+    opacity: [0,1],
+    easing: "easeOutCirc",
+    duration: 800,
+    delay: (el, i) => 800 * i
+  }).add({
+    targets: '.ml15',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+</script>
+<!-- end-text-effect -->
+
+<!-- cuvantul-ierarhului -->
+<section class="card-container">
+  <div id="card">
+    <figure class="front"><div class="left-div">
+      <p class="content-name">cole gwoz blog</p>
+      <p class="content-title">C.</p>
+      <p class="content-description">j'aime cole et du chocolat!
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu quam<br/> quis enim malesuada ullamcorper nec at est. Aliquam erat volutpat.</p>
+			<section id="explore">
+        <p><a id="flip">Explore My Blog</a></p>
+      </section>
+  </div>
+  <div class="right-div">
+    <img src="../protopopiatul agnita/assets/img/parohii/ips-laurentiu-streza-2017-07-696x400.jpg" alt="" style="width: 17rem;" >
+
+      </div>
+    </figure>
+    <!--<figure class="back"></figure>!-->
+  </div>
+</section>
+<p style="position:absolute;
+    bottom:0;
+    right:20px; font-family: 'Open Sans', sans-serif;
+  letter-spacing: 1px;
+          font-size: 12px; font-weight:300; color:#6e8786">Created by Cole Gwoz</p>
+
+          <style>
+            @import url('https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i,900,900i');
+@import url('https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i');
+
+.hero {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  background-color:#cedddc; /*f2f2f2*/}
+
+#card {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  transform-style: preserve-3d;
+  transition: transform 1s;}
+
+#card figure {
+  margin: 0;
+  display: block;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;}
+
+#card .front {
+  background: transparent;}
+
+#card .back {
+  background: white;
+  transform: rotateY( 180deg );}
+
+#card.flipped {
+  transform: rotateY( 180deg );}
+
+.card-container {
+  width: 900px;
+  height: 550px;
+  position: absolute;
+  perspective: 800px;
+	margin: 20px auto;
+  top:0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+	transform:scale(0.9);}
+
+.left-div {
+  width:361px;
+  height:550px;
+  background-color:white;
+  position: absolute;
+  top:0;
+  bottom: 0;
+  left: 0;
+  margin: auto;}
+
+.right-div {
+  width:540px;
+  height:550px;
+  transform:scaleX(-1);
+  background-position: -170px 0px;
+  background-size:cover;
+  position: absolute;
+  top:0;
+  bottom: 0;
+  right: 0;
+  margin: auto;}
+
+.content-name {
+  font-family: 'Open Sans', sans-serif;
+  letter-spacing: 3px;
+  font-size: 10px;
+  text-transform: uppercase;
+  color: #7E7E7E;
+  font-weight: 700;
+  margin-top:160px;
+  margin-left:40px;}
+
+.content-title {
+  font-family: 'Playfair Display', serif;
+  font-size: 44px;
+  letter-spacing: 3px;
+  font-weight: 700;
+  color: #2C2C2C;
+  margin-top:10px;
+  margin-left:40px;}
+
+.content-description {
+  margin-top: -20px;
+  font-family: 'Open Sans', sans-serif;
+  font-size: 13px;
+  color: #7e7e7e;
+  line-height: 22px;
+  margin-left:40px;}
+
+#explore a {
+	position:absolute;
+  margin-top:9px;
+  color: #2C2C2C;
+  font-family: 'Open Sans', sans-serif;
+  letter-spacing: 3px;
+  font-size: 11px;
+  text-transform: uppercase;
+  font-weight: 700;
+  text-decoration: none;
+  margin-left:40px;
+  background-color:transparent;
+  border:none;
+  cursor:pointer;
+  background-color:yellow;
+  padding:12px 12px 12px 12px;
+  margin-left:28px;}
+
+
+
+          </style>
+<!-- cuvantul-ierarhului -->
+
+
+    <!-- <div
       class="container"
       data-aos="zoom-out"
       data-aos-delay="100"
@@ -322,5 +517,5 @@
       <h1>Protopopiatul Ortodox Român <span>Agnita</span></h1>
       <h2><i>De Ma iubiti, paziti poruncile Mele.</i> (Ioan 14, 15)</h2>
 
-    </div>
+    </div> -->
   </section><!-- End Hero -->
